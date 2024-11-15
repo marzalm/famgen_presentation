ed786f1576008ecd2c3ac1fbc8943e6bc96f5e1a
unknown <marzal.marius@gmail.com>
2024-11-15 08:37:05 +0100

    Commit initial: famille generation

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import SurveyPresentation from './components/SurveyPresentation';
+
+function App() {
+  return (
+    <div className="App">
+      <SurveyPresentation />
+    </div>
+  );
+}
+
+export default App;
